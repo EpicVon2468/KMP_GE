30e4f132e0d967e0b2e551db126cb5358e0db81c
EpicVon2468 <thomaschad0909@gmail.com>
2025-10-21 16:36:11 +1100

    counter conversion

--- core/src/commonMain/kotlin/io/github/epicvon2468/core/interop/glfw/GLFWFunctions.kt
+++ core/src/commonMain/kotlin/io/github/epicvon2468/core/interop/glfw/GLFWFunctions.kt
@@ -383,5 +383,12 @@ expect fun glfwWindowShouldClose(window: GLFWWindow?): Boolean
 inline fun Int.glfwBoolean(): Boolean = when (this) {
 	GLFW_TRUE -> true
 	GLFW_FALSE -> false
-	else -> error("Couldn't parse glfw boolean '$this', expected either '${GLFW_TRUE}' (true) or '${GLFW_FALSE}' (false)!")
+	else -> error("Couldn't parse glfw boolean '$this', expected either '$GLFW_TRUE' (true) or '$GLFW_FALSE' (false)!")
+}
+
+@GLFW
+@Suppress("NOTHING_TO_INLINE")
+inline fun Boolean.glfwBoolean(): Int = when (this) {
+	true -> GLFW_TRUE
+	false -> GLFW_FALSE
 }
