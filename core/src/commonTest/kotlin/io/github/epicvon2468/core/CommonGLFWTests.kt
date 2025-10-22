5d64d5ff721713c5894e9fa2bbadb187999a5715
EpicVon2468 <thomaschad0909@gmail.com>
2025-10-22 18:12:48 +1100

    Error tests

--- core/src/commonTest/kotlin/io/github/epicvon2468/core/CommonGLFWTests.kt
+++ core/src/commonTest/kotlin/io/github/epicvon2468/core/CommonGLFWTests.kt
@@ -6,11 +6,14 @@ import io.github.epicvon2468.core.interop.glfw.init.glfwInit
 import io.github.epicvon2468.core.interop.glfw.init.glfwSetErrorCallback
 import io.github.epicvon2468.core.interop.glfw.init.glfwTerminate
 import io.github.epicvon2468.core.interop.glfw.window.GLFWWindow
+import io.github.epicvon2468.core.interop.glfw.window.GLFW_CONTEXT_VERSION_MAJOR
+import io.github.epicvon2468.core.interop.glfw.window.GLFW_CONTEXT_VERSION_MINOR
 import io.github.epicvon2468.core.interop.glfw.window.glfwCreateWindow
 import io.github.epicvon2468.core.interop.glfw.window.glfwDestroyWindow
 import io.github.epicvon2468.core.interop.glfw.window.glfwPollEvents
 import io.github.epicvon2468.core.interop.glfw.window.glfwSetWindowShouldClose
 import io.github.epicvon2468.core.interop.glfw.window.glfwSwapBuffers
+import io.github.epicvon2468.core.interop.glfw.window.glfwWindowHint
 import io.github.epicvon2468.core.interop.glfw.window.glfwWindowShouldClose
 
 import kotlin.test.Test
@@ -20,11 +23,11 @@ import kotlin.time.Clock
 import kotlin.time.Duration.Companion.seconds
 import kotlin.time.ExperimentalTime
 
-class CommonWindowTest {
+class CommonGLFWTests {
 
 	@Test
 	@OptIn(ExperimentalTime::class)
-	fun commonWindowTest() {
+	fun windowTest() {
 		assertTrue(glfwInit(), "GLFW failed to initialise!")
 		glfwSetErrorCallback { errorCode: Int, description: String? ->
 			fail("GLFW had an error! Error was: (errorCode: '$errorCode', description: '$description').")
@@ -48,5 +51,19 @@ class CommonWindowTest {
 
 		glfwDestroyWindow(window)
 		glfwTerminate()
+	}
+
+	@Test
+	fun errorTest() {
+		assertTrue(glfwInit(), "GLFW failed to initialise!")
+		var fail = true
+		glfwSetErrorCallback { errorCode: Int, description: String? ->
+			println("Caught successful error, terminating!")
+			fail = false
+		}
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 99)
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 99)
+		glfwCreateWindow(1, 1, "error")
+		if (fail) fail("Error callback should have redefined, but is still going?")
 	}
 }
